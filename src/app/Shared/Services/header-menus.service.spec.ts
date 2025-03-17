c85dc9b3f22878e160a5acee2c5bfd16021eaa33
oHeviaDeveloper <ohevia@uoc.edu>
2025-03-17 15:50:41 +0100

    header menu service unit testing finished

--- src/app/Shared/Services/header-menus.service.spec.ts
+++ src/app/Shared/Services/header-menus.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-
 import { HeaderMenusService } from './header-menus.service';
+import { HeaderMenus } from '../Models/header-menus.dto';
 
 describe('HeaderMenusService', () => {
   let service: HeaderMenusService;
@@ -12,5 +12,26 @@ describe('HeaderMenusService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have initial header state with showAuthSection: false and showNoAuthSection: true', () => {
+    service.headerManagement.subscribe((menuState: HeaderMenus) => {
+      expect(menuState.showAuthSection).toBeFalse();
+      expect(menuState.showNoAuthSection).toBeTrue();
+    });
+  });
+
+  it('should update header state correctly', () => {
+    const newState: HeaderMenus = {
+      showAuthSection: true,
+      showNoAuthSection: false,
+    };
+
+    service.headerManagement.next(newState);
+
+    service.headerManagement.subscribe((menuState: HeaderMenus) => {
+      expect(menuState.showAuthSection).toBeTrue();
+      expect(menuState.showNoAuthSection).toBeFalse();
+    });
   });
 });
